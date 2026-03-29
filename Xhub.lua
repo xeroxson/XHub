722e5ee673f468081ae8faa19ccadfaa0588ebe0
xeroxson <artekty2@gmail.com>
2026-03-29 20:47:00 +0400

    XHub.lua

--- Xhub.lua
+++ Xhub.lua
@@ -1,2 +1,6 @@
+if game.PlaceId == 920587237 then
 loadstring(game:HttpGet("https://pastefy.app/sIfsKoYw/raw"))()
+end
+if game.PlaceId == 142823291 then
 loadstring(game:HttpGet("https://pastefy.app/PrXpxsIH/raw"))()
+end
