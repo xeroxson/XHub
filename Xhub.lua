79de75651a51497c48006b3ddfff807e36ab715c
xeroxson <artekty2@gmail.com>
2026-03-30 16:55:49 +0400

    Xhub.lua

--- Xhub.lua
+++ Xhub.lua
@@ -1,3 +1,3 @@
-loadstring(game:HttpGet("https://pastefy.app/sIfsKoYw/raw"))()
+loadstring(game:HttpGet("https://pastefy.app/S1U0JRXy/raw"))()
 loadstring(game:HttpGet("https://pastefy.app/PrXpxsIH/raw"))()
 
