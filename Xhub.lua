52a9b904b34af730cb830d9d3823cdcbbc2e0311
xeroxson <artekty2@gmail.com>
2026-04-01 19:38:39 +0400

    Xhub.lua

--- Xhub.lua
+++ Xhub.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://pastefy.app/oJpYafO3/raw"))()
+loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6ImMwNmY5ZDhlLTY3NzctNDcyYy04ZjMwLWU3MThlZGIwM2MyOSIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJ1bml2ZXJzYWwifX0"))()
 loadstring(game:HttpGet("https://pastefy.app/uaGyYqgk/raw"))()
